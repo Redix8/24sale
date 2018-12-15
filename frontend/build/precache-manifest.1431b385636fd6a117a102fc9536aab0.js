4599b0639ab25d04b50a4ea143ed7f6c39f71ada
Redix8 <40425965+Redix8@users.noreply.github.com>
2018-12-15 23:20:14 +0900

    production add

---
+++ frontend/build/precache-manifest.1431b385636fd6a117a102fc9536aab0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69f48a2e948b7b9ae27f",
+    "url": "/static/js/main.69f48a2e.chunk.js"
+  },
+  {
+    "revision": "0b72805a31553db143de",
+    "url": "/static/js/1.0b72805a.chunk.js"
+  },
+  {
+    "revision": "69f48a2e948b7b9ae27f",
+    "url": "/static/css/main.3c452682.chunk.css"
+  },
+  {
+    "revision": "b7a209bb23e96f823fabfaec7c78ad0e",
+    "url": "/index.html"
+  }
+];
